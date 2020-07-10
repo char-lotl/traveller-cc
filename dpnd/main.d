0e2abf61cf5b7ade90353725d926c3d85348c0d9
Charlotte Lastname <lancharlotl@gmail.com>
2020-07-09 22:08:21 -0400

    Removing obj and dpnd files from repo.

--- dpnd/main.d
+++
@@ -1,4 +0,0 @@
-obj/./main.o dpnd/main.d : src/main.cpp src/skills/Repertoire.h src/skills/skill_type.h \
-  src/config/Rules.h src/score_assign/assign_characteristic_scores.h \
-  src/config/read_in_preferences.h \
-  src/background/pick_background_skills.h
